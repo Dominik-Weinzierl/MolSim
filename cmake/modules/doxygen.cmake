7f821b1583d05b597399afc88f6ba2c9006b70fb
Dominik Weinzierl <dominik.weinzierl@mailbox.org>
2021-10-29 14:31:07 +0200

    Implement doxygen support

--- cmake/modules/doxygen.cmake
+++ cmake/modules/doxygen.cmake
@@ -1,5 +1,29 @@
 # make doc_doxygen optional if someone does not have / like doxygen
 
-# TODO: create CMake build option for the target.
+option(BUILD_DOCUMENTATION "Build documentation of project" OFF)
 
-# TODO: Add a custom target for building the documentation.
+# check if Doxygen is installed
+find_package(Doxygen)
+
+if (BUILD_DOCUMENTATION)
+    if (DOXYGEN_FOUND)
+        # set input and output files
+        set(DOXYGEN_IN ${CMAKE_CURRENT_SOURCE_DIR}/docs/doxyfile)
+        set(DOXYGEN_OUT ${CMAKE_CURRENT_BINARY_DIR}/doxyfile)
+
+        # request to configure the file
+        configure_file(${DOXYGEN_IN} ${DOXYGEN_OUT} @ONLY)
+        message("Doxygen build started")
+
+        # note the option ALL which allows to build the docs together with the application
+        add_custom_target(doc_doxygen ALL
+                COMMAND ${DOXYGEN_EXECUTABLE} ${DOXYGEN_OUT}
+                WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR})
+
+        # option to remove documentation
+        add_custom_target(clean_doxygen
+                COMMAND rm -rf ${CMAKE_CURRENT_BINARY_DIR}/doxys_documentation)
+    else (DOXYGEN_FOUND)
+        message("Doxygen need to be installed to generate the doxygen documentation")
+    endif (DOXYGEN_FOUND)
+endif (BUILD_DOCUMENTATION)
