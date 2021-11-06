9a8392e8a5d029bf8b499962c5b4880a8c000603
Dominik Weinzierl <dominik.weinzierl@mailbox.org>
2021-11-06 10:07:48 +0100

    Make cmake more modular

--- cmake/modules/doxygen.cmake
+++ cmake/modules/doxygen.cmake
@@ -6,22 +6,22 @@ option(BUILD_DOCUMENTATION "Build documentation of project" OFF)
 find_package(Doxygen)
 
 if (BUILD_DOCUMENTATION)
-    if (DOXYGEN_FOUND)
-        # set input and output files
-        set(DOXYGEN_IN ${CMAKE_CURRENT_SOURCE_DIR}/docs/doxyfile)
-        set(DOXYGEN_OUT ${CMAKE_CURRENT_BINARY_DIR}/doxyfile)
+  if (DOXYGEN_FOUND)
+    # set input and output files
+    set(DOXYGEN_IN ${CMAKE_CURRENT_SOURCE_DIR}/docs/doxyfile)
+    set(DOXYGEN_OUT ${CMAKE_CURRENT_BINARY_DIR}/doxyfile)
 
-        configure_file(${DOXYGEN_IN} ${DOXYGEN_OUT} @ONLY)
+    configure_file(${DOXYGEN_IN} ${DOXYGEN_OUT} @ONLY)
 
-        # note the option ALL which allows to build the docs together with the application
-        add_custom_target(doc_doxygen ALL
-                COMMAND ${DOXYGEN_EXECUTABLE} ${DOXYGEN_OUT}
-                WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR})
+    # note the option ALL which allows to build the docs together with the application
+    add_custom_target(doc_doxygen ALL
+        COMMAND ${DOXYGEN_EXECUTABLE} ${DOXYGEN_OUT}
+        WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR})
 
-        # option to remove documentation
-        add_custom_target(clean_doxygen
-                COMMAND rm -rf ${CMAKE_CURRENT_BINARY_DIR}/doxys_documentation)
-    else (DOXYGEN_FOUND)
-        message("Doxygen need to be installed to generate the doxygen documentation")
-    endif (DOXYGEN_FOUND)
+    # option to remove documentation
+    add_custom_target(clean_doxygen
+        COMMAND rm -rf ${CMAKE_CURRENT_BINARY_DIR}/doxys_documentation)
+  else (DOXYGEN_FOUND)
+    message("Doxygen need to be installed to generate the doxygen documentation")
+  endif (DOXYGEN_FOUND)
 endif (BUILD_DOCUMENTATION)
