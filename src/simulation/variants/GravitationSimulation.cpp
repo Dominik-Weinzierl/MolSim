d2308c9ae3fbe31058b2fcc656e0a980a43f45a9
NiOHar <garfieldgbc@gmail.com>
2021-11-06 16:39:41 +0100

    replace the last remaining cout

--- src/simulation/variants/GravitationSimulation.cpp
+++ src/simulation/variants/GravitationSimulation.cpp
@@ -1,8 +1,9 @@
+#include <spdlog/spdlog.h>
 #include "GravitationSimulation.h"
 
 void GravitationSimulation::performSimulation(Argument &arg, OutputWriter &writer,
                                               ParticleContainer &particleContainer) {
   Gravitation gravitation;
   Simulation::performSimulation(writer, gravitation, particleContainer, arg);
-  std::cout << "output written. Terminating..." << std::endl;
+  SPDLOG_INFO("Output written, terminating");
 }
