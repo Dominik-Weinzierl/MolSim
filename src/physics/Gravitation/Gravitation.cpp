e963a8c045e02cd0e70f383f531454d6cf1885e6
Dominik Weinzierl <dominik.weinzierl@mailbox.org>
2021-11-18 19:43:34 +0100

    Revert changes

--- src/physics/Gravitation/Gravitation.cpp
+++ src/physics/Gravitation/Gravitation.cpp
@@ -34,8 +34,7 @@ void Gravitation<2>::performUpdate(ParticleContainer<2> &particleContainer) cons
       double y;
       double m = i->getM() * j->getM();
 
-      // Since 2D calculations for gravitation are wrong, we need to wrap with 3D.
-      Vector<3> difference{j->getX()[0] - i->getX()[0], j->getX()[1] - i->getX()[1], 0};
+      Vector<2> difference = j->getX() - i->getX();
       double l2Norm = ArrayUtils::L2Norm(difference);
 
       double factor = m / (l2Norm * l2Norm * l2Norm);
