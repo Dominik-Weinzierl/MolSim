7c94d6555e020bd15cd45065ebb4507a11d01e7c
Dominik Weinzierl <github.weinzierl@mailbox.org>
2021-12-08 10:10:24 +0100

    Update tests

--- tests/container/LinkedCell/LinkedCellContainer.cpp
+++ tests/container/LinkedCell/LinkedCellContainer.cpp
@@ -8,7 +8,7 @@
  * function getIndexBasedOnCoordinates, 2D
  */
 TEST(LinkedCellContainer_2D, checkMeshProperties) {
-  LinkedCellContainer<2> l{{Outflow, Outflow, Outflow, Outflow}, {1,1}, {3,3}, 1.5};
+  LinkedCellContainer<2> l{{Outflow, Outflow, Outflow, Outflow}, {1, 1}, {3, 3}, 1.5};
   l.init();
 
   //Test size
@@ -17,13 +17,12 @@ TEST(LinkedCellContainer_2D, checkMeshProperties) {
   //Test getIndexBasedOnCoordinates
   auto i = 0;
 
-  for(auto x = -1; x < l.getDomain()[0] + 1; x++){
-    for(auto y = -1; y < l.getDomain()[1] + 1; y++){
-      EXPECT_EQ(i, l.getIndexBasedOnCoordinates(
-          Vector<2>{static_cast<double>(x), static_cast<double>(y)}));
-        i++;
-      }
+  for (auto x = -1; x < l.getDomain()[0] + 1; x++) {
+    for (auto y = -1; y < l.getDomain()[1] + 1; y++) {
+      EXPECT_EQ(i, l.getIndexBasedOnCoordinates(Vector<2>{static_cast<double>(x), static_cast<double>(y)}));
+      i++;
     }
+  }
 }
 
 /**
@@ -31,7 +30,7 @@ TEST(LinkedCellContainer_2D, checkMeshProperties) {
  * function getIndexBasedOnCoordinates, 3D
  */
 TEST(SmallLinkedCellContainer_3D, checkMeshProperties) {
-  LinkedCellContainer<3> l{{Outflow, Outflow, Outflow, Outflow, Outflow, Outflow}, {1,1, 1}, {3,3, 3}, 1.5};
+  LinkedCellContainer<3> l{{Outflow, Outflow, Outflow, Outflow, Outflow, Outflow}, {1, 1, 1}, {3, 3, 3}, 1.5};
   l.init();
 
   //Test size
@@ -40,9 +39,9 @@ TEST(SmallLinkedCellContainer_3D, checkMeshProperties) {
   //Test getIndexBasedOnCoordinates
   auto i = 0;
 
-  for(auto z = -1; z < l.getDomain()[2] + 1; z++){
-    for(auto x = -1; x < l.getDomain()[0] + 1; x++){
-      for(auto y = -1; y < l.getDomain()[1] + 1; y++){
+  for (auto z = -1; z < l.getDomain()[2] + 1; z++) {
+    for (auto x = -1; x < l.getDomain()[0] + 1; x++) {
+      for (auto y = -1; y < l.getDomain()[1] + 1; y++) {
         EXPECT_EQ(i, l.getIndexBasedOnCoordinates(
             Vector<3>{static_cast<double>(x), static_cast<double>(y), static_cast<double>(z)}));
         i++;
@@ -56,7 +55,7 @@ TEST(SmallLinkedCellContainer_3D, checkMeshProperties) {
  * function getIndexBasedOnCoordinates, 3D
  */
 TEST(HugeLinkedCellContainer_3D, checkMeshProperties) {
-  LinkedCellContainer<3> l{{Outflow, Outflow, Outflow, Outflow, Outflow, Outflow}, {2,1, 3}, {24,24, 27}, 3.5};
+  LinkedCellContainer<3> l{{Outflow, Outflow, Outflow, Outflow, Outflow, Outflow}, {2, 1, 3}, {24, 24, 27}, 3.5};
   l.init();
 
   //Test size
@@ -65,9 +64,9 @@ TEST(HugeLinkedCellContainer_3D, checkMeshProperties) {
   //Test getIndexBasedOnCoordinates
   auto i = 0;
 
-  for(auto z = -3; z < l.getDomain()[2] + 1; z+=3){
-    for(auto x = -2; x < l.getDomain()[0] + 1; x+=2){
-      for(auto y = -1; y < l.getDomain()[1] + 1; y+=1){
+  for (auto z = -3; z < l.getDomain()[2] + 1; z += 3) {
+    for (auto x = -2; x < l.getDomain()[0] + 1; x += 2) {
+      for (auto y = -1; y < l.getDomain()[1] + 1; y += 1) {
         EXPECT_EQ(i, l.getIndexBasedOnCoordinates(
             Vector<3>{static_cast<double>(x), static_cast<double>(y), static_cast<double>(z)}));
         i++;
@@ -82,14 +81,17 @@ TEST(HugeLinkedCellContainer_3D, checkMeshProperties) {
 TEST(Boundary_2D, checkReflection) {
   LinkedCell<LennardJones, 2> linkedCell{};
   LinkedCellContainer<2> l{{Reflecting, Reflecting, Reflecting, Reflecting}, {2, 1}, {180, 90}, 3.0};
-  l.init();
   ASSERT_TRUE(l.size() == 0);
+
   l.addParticle({{20, 20}, {0.0, -10.0}, 1.0});
-  std::cout << l.getParticles()[0].getX() << std::endl;
+
+  l.init();
+
   linkedCell.calculateNextStep(l, 0.0005);
+
   ASSERT_TRUE(l.size() == 1);
+
   for (auto i = 0; i < 10000; i++) {
-    std::cout << l.getParticles()[0].getX() << std::endl;
     linkedCell.calculateNextStep(l, 0.0005);
   }
 }
@@ -101,8 +103,11 @@ TEST(Halo_2D, checkOutflow) {
   LinkedCell<LennardJones, 2> linkedCell{};
   LinkedCellContainer<2> l{{Outflow, Outflow, Outflow, Outflow}, {1, 1}, {3, 3}, 1.0};
   ASSERT_TRUE(l.size() == 0);
+
   l.addParticle({{0.1, 0.1}, {-1.0, 0.0}, 1.0});
+
   l.init();
+
   linkedCell.calculateNextStep(l, 0.5);
   ASSERT_TRUE(l.size() == 0);
 }
@@ -110,7 +115,7 @@ TEST(Halo_2D, checkOutflow) {
 /**
 * Checks behavior when the CutOffRadius is greater than the cell size, 2D
 */
-TEST(LinkedCellContainer_2D, checkCutoffRadius){
+TEST(LinkedCellContainer_2D, checkCutoffRadius) {
   LinkedCell<LennardJones, 2> linkedCell{};
   LinkedCellContainer<2> l{{Outflow, Outflow, Outflow, Outflow}, {1, 1}, {3, 3}, 2};
 
@@ -128,13 +133,14 @@ TEST(LinkedCellContainer_2D, checkCutoffRadius){
 /**
 * Checks behavior when the CutOffRadius is greater than the cell size, 3D
 */
-TEST(LinkedCellContainer_3D, checkCutoffRadius){
+TEST(LinkedCellContainer_3D, checkCutoffRadius) {
   LinkedCell<LennardJones, 3> linkedCell{};
   LinkedCellContainer<3> l{{Outflow, Outflow, Outflow, Outflow, Outflow, Outflow}, {1, 1, 1}, {3, 3, 3}, 1.5};
-  l.init();
 
   l.addParticle({{0.1, 0.1, 0.1}, {-1.0, 0.0, 0.0}, 1.0});
   l.addParticle({{1.5, 1.5, 1.5}, {-1.0, 0.0, 0.0}, 1.0});
+
+  l.init();
 
   auto force = l.getParticles()[1].getF();
   linkedCell.calculateNextStep(l, 1.0);
