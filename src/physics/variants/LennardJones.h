14358e18176f4490e8a4799d7fe8fd3312a58aa1
Dominik Weinzierl <github.weinzierl@mailbox.org>
2021-12-14 23:11:17 +0100

    Add mixing rule and add first example file for week 4

--- src/physics/variants/LennardJones.h
+++ src/physics/variants/LennardJones.h
@@ -14,9 +14,13 @@ class LennardJones : public PhysicsType {
   static inline Vector<dim> calculateForceBetweenTwoParticles(Particle<dim> &i, Particle<dim> &j, double &l2Norm) {
     Vector<dim> diff{i.getX() - j.getX()};
 
-    double fracture = (i.getZeroCrossing() * j.getZeroCrossing()) / l2Norm;
+    // Lorentz-Berthelot mixing rule
+    double zeroCrossing = (i.getZeroCrossing() + j.getZeroCrossing()) / 2;
+    double potentialWellDepth = std::sqrt(i.getPotentialWellDepth() * j.getPotentialWellDepth());
 
-    double firstFactor = (24 * i.getPotentialWellDepth()) / l2Norm;
+    double fracture = (zeroCrossing * zeroCrossing) / l2Norm;
+
+    double firstFactor = (24 * potentialWellDepth) / l2Norm;
     double pow = fracture * fracture * fracture;
     double secondFactor = pow - 2 * pow * pow;
     double factor = firstFactor * secondFactor;
