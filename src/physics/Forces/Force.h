36d22467e99799eab05a8c767d69afe0e3647cf1
Dominik Weinzierl <github.weinzierl@mailbox.org>
2022-01-06 11:06:36 +0100

    remove to string

--- src/physics/Forces/Force.h
+++ src/physics/Forces/Force.h
@@ -97,8 +97,7 @@ class Force {
   }
 
   friend std::ostream &operator<<(std::ostream &os, const Force &f) {
-    os << "indices: " << ArrayUtils::to_string(f.indices) << " additionalForceParticles: " << ArrayUtils::to_string(f.additionalForceParticles) << " force: "
-       << ArrayUtils::to_string(f.force) << " startTime: " << f.startTime << " endTime: " << f.endTime;
+    os << "indices: ";
     return os;
   }
 };
